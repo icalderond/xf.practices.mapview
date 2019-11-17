4a7df4981e7372da63b8dd9fd1131d9633a829ae
Israe Calderón <icalderond@outlook.com>
2019-11-17 10:33:07 -0600

    Initial Commit

---
+++ xc.practices.MKMapView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xc.practices.MKMapView
+//
+//  Created by Israel Calderon de la Cruz on 11/17/19.
+//  Copyright © 2019 Israel Calderon de la Cruz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
